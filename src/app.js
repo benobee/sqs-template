30cc94e60443c63449a94a3a91651083c9cb87c0
Ben Obee <benobee@gmail.com>
2018-04-10 13:00:46 -0700

    api folder, new controller

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
-import Controller from "./core/controller";
+import controller from "./core/controller";
+import api from "./api/api";
 
 const App = {
     init () {
-        this.blog = new Controller("blog", {
-            el: ".blog",
-            rendered (el) {
-                // run code
-                console.log("hello blog", el);
-            }
+        this.api = api();
+        this.registerControllers();
+    },
+
+    /**
+     * events are bound to the controller when
+     * elements are found within the DOM.
+     */
+    registerControllers () {
+        controller.add({
+            name: "blog",
+            el: "#collection-5a81023c53450a498144b8d7"
         });
+        controller.watch();
     }
 };
 
--- src/core/controller.js
+++ src/core/controller.js
@@ -1,23 +1,44 @@
-const controller = (controllerName, config) => {
+import PubSub from "./pubsub";
+
+const events = new PubSub();
+
+class Controller {
+    constructor () {
+        this.topics = {};
+        this.on = events.on;
+        this.emit = events.emit;
+        this.events = [];
+        this.add = this.add;
+    }
+
+    add (config) {
+        this.events.push(config);
+    }
 
     /**
      * Tests whether the node is active in the DOM
      * @param  {String} query query selector
      * @returns {Object}       DOM Node
      */
-    const elementIsActive = (query) => {
+
+    elementIsActive (query) {
         const el = document.querySelector(query);
 
         if (!el) {
             return false;
         }
-        config.rendered(el);
         return el;
-    };
+    }
 
-    return {
-        el: elementIsActive(config.el)
-    };
-};
+    watch () {
+        this.events.forEach((event) => {
+            const el = this.elementIsActive(event.el);
 
-export default controller;
+            this.emit(event.name, el);
+        });
+    }
+}
+
+const instance = new Controller();
+
+export default instance;

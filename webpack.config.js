a43b034bcfb5916672d6fc9e674ed26b18210d78
Ben Obee <benobee@gmail.com>
2018-06-25 22:06:59 -0700

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,24 +6,9 @@ const rules = require("./config/webpack/rules.js");
 const plugins = require("./config/webpack/plugins.js");
 const output = require("./config/webpack/output.js");
 
-/****************************************/
-/*******     CONFIG OBJECT      *********/
-/****************************************/
-
 const WEBPACK_CONFIG = {
     module: {}
 };
-
-/****************************************/
-/*******      ENVRIONMENTS      *********/
-/****************************************/
-
-/*
- * test whether the script will be run in
- * production using "npm run build" from
- * the terminal. If true, file names will
- * be hashed, js will be minified.
- */
 
 //extend properties to config
 Object.assign(WEBPACK_CONFIG, input);
